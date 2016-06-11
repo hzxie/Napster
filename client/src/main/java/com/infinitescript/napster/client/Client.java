239f8303bcd7dd695faca27e085e7f4a16073cb5
Haozhe Xie <zjhzxhz@gmail.com>
2016-06-11 22:47:46 +0800

    Fix import statements in client.

--- client/src/main/java/com/infinitescript/napster/client/Client.java
+++ client/src/main/java/com/infinitescript/napster/client/Client.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 import com.alibaba.fastjson.JSON;
